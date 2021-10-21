a9feafc3f668cb977634be9bcdbf4328ef51cc49
FairaIchsa <zelov02@yandex.ru>
2021-10-21 17:52:46 +0300

    added category filter and PageNumberPagination for ProductListApiView

--- mainapp/api/product/api_views.py
+++ mainapp/api/product/api_views.py
@@ -1,12 +1,38 @@
+from collections import OrderedDict
+
+from rest_framework.response import Response
 from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.pagination import PageNumberPagination
 
 from .serializers import *
 from ...models import *
 
 
+class ProductListPagination(PageNumberPagination):
+    page_size = 20
+
+    def get_paginated_response(self, data):
+        return Response(OrderedDict([
+            ('page', self.page.number),
+            ('page_size', self.page_size),
+            ('page_count', self.page.paginator.num_pages),
+            ('next', self.get_next_link()),
+            ('previous', self.get_previous_link()),
+            ('items', data)
+        ]))
+
+
 class ProductListApiView(ListAPIView):  # добавить пагинацию
     queryset = Product.objects.all()
     serializer_class = ProductListSerializer
+    pagination_class = ProductListPagination
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        category = self.request.query_params.get('category')
+        if category:
+            return queryset.filter(category__slug=category)
+        return queryset
 
 
 class ProductRetrieveApiView(RetrieveAPIView):
--- mainapp/api/user/api_views.py
+++ mainapp/api/user/api_views.py
@@ -1,9 +1,7 @@
 from rest_framework.views import APIView
-from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 from .serializers import *
-from ...models import *
 
 
 class UserAPIView(APIView):
