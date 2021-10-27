9ae0fcc57882c7da9cdb3b2c7cd0348886320c9d
FairaIchsa <zelov02@yandex.ru>
2021-10-27 15:33:21 +0300

    updated User model and UserSerializer

--- mainapp/api/user/serializers.py
+++ mainapp/api/user/serializers.py
@@ -4,6 +4,15 @@ from ...models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    firstname = serializers.CharField(source='first_name')
+    secondname = serializers.CharField(source='last_name')
+    fathername = serializers.CharField(source='father_name')
+    ship_adress = serializers.CharField(source='ship_address')
+    is_anonymous = serializers.BooleanField(source='anonymous')
+
     class Meta:
         model = User
-        fields = '__all__'
+        fields = [
+            'id', 'username', 'firstname', 'secondname', 'fathername', 'phone', 'email', 'ship_adress',
+            'birthday', 'is_anonymous',
+        ]
--- mainapp/models.py
+++ mainapp/models.py
@@ -3,13 +3,13 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    # # уже есть id, username, password, first_name, last_name, email,
-    # # is_superuser, is_staff, is_active, last_login, date_joined
-    # father_name = models.CharField(max_length=150, blank=True)
-    # birthday = models.DateField(blank=True)
-    # phone = models.CharField(max_length=15, blank=True, unique=True)
-    # ship_address = models.CharField(max_length=255, blank=True)
-    # is_anonymous = models.BooleanField(default=False)  # is_anonymous занято каким-то методом и джанго ругается
+    # уже есть id, username, password, first_name, last_name, email,
+    # is_superuser, is_staff, is_active, last_login, date_joined
+    father_name = models.CharField(max_length=150, blank=True)
+    birthday = models.DateField(blank=True, null=True)
+    phone = models.CharField(max_length=15, blank=True, unique=True)
+    ship_address = models.CharField(max_length=255, blank=True)
+    anonymous = models.BooleanField(default=False)  # is_anonymous занято каким-то методом и джанго ругается
 
     def __str__(self):
         return self.username
