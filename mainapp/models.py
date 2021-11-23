72a3c4ac91658df50ec03b01d6313141b71e67bc
FairaIchsa <zelov02@yandex.ru>
2021-11-23 19:25:48 +0300

    modified User model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,15 +1,56 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
-    # уже есть id, username, password, first_name, last_name, email,
-    # is_superuser, is_staff, is_active, last_login, date_joined
+    username = None
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=150, blank=True, null=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
     father_name = models.CharField(max_length=150, blank=True, null=True)
     birthday = models.DateField(blank=True, null=True)
-    phone = models.CharField(max_length=15, blank=True, unique=True)
+    phone = models.CharField(max_length=15, blank=True)
     ship_address = models.CharField(max_length=255, blank=True)
     anonymous = models.BooleanField(default=False)  # is_anonymous занято каким-то методом и джанго ругается
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
     def __str__(self):
-        return self.username
+        return self.email
